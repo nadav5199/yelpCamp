c5ead4600a5d93164f728786095a20722e5aea35
Nadav passal <nadav.passal@post.runi.ac.il>
2024-01-20 19:16:40 +0200

    index page is working

--- app.js
+++ app.js
@@ -11,21 +11,19 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Database connected');
 });
-
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-
+app.get('/campgrounds',async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+})
 app.get('/', (req, res) => {
     res.render('home');
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My back yard', description:'a very nice'});
-    await camp.save();
-    res.send(camp);
-})
+
 app.listen(3000, () => {
     console.log('serving on port 3000');
 })
