0a91efe3a4e9fb5fc610684a69e2f424adb55b0a
Nadav passal <nadav.passal@post.runi.ac.il>
2024-01-27 13:27:19 +0200

    added an edit page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campgrounds');
+const { AsyncLocalStorage } = require('async_hooks');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
@@ -17,27 +18,36 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
 
-
+//home page
 app.get('/', (req, res) => {
     res.render('home');
 })
+//show page
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
 })
+//create a new campground form
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
-
+//creates a new campground
 app.post('/campgrounds',async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
 })
+//show page for the specific campground
 app.get('/campgrounds/:id',async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
 })
+//edit page for the campground
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+//opens up a port
 app.listen(3000, () => {
     console.log('serving on port 3000');
 })
